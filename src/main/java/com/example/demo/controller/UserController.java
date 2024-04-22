4c3630695292ea33976bb727d351a668c8b2fa67
Jatin Sharma <76206132+12602@users.noreply.github.com>
2024-04-22 20:36:32 +0530

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,101 +15,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Quiz;
 import com.example.demo.entity.User;
-import com.example.demo.service.QuizService;
+import com.example.demo.entity.UserQuiz;
 import com.example.demo.service.UserService;
 
 @RestController
-@RequestMapping("/quiz")
-@CrossOrigin("*")
-public class QuizController {
-	     @Autowired
-	      QuizService quiz;
-	     @Autowired
-	       UserService user;
-	     @Autowired
-	     private ModelMapper modelMapper;
-
-	 @GetMapping("/getQuiz/{cat}")
-
-	 public List<Quiz> getQuiz(@PathVariable("cat")String cat){
-		    
-		 
-		 List<Quiz>li= quiz.getQuiz(cat);
-		   Collections.shuffle(li);
-		   System.out.println(cat);
-		   if(li.size()>=10) {
-			   return 	 li.subList(0, 10); 
-		   }
-		   else
-			   return li;
-			   
-		 
-		 
-	 }
-	 @GetMapping("/getCompleteQuiz/{cat}")
-	
-	 public List<Quiz> getCompleteQuiz(@PathVariable("cat")String cat){
-		    
-		 
-		 List<Quiz>li= quiz.getQuiz(cat);
-		 
-		 
-			   return li;
-			   
-		 
-		 
-	 }
-	 
-	 @PostMapping("/addQuiz")
-	 
-	 public List<Quiz> addQuiz(@RequestBody Map<String, Object> payload) 
-	 {
-		  System.out.println(payload);
-		 ArrayList<Object>q=(ArrayList<Object>)payload.get("quiz");
-		 ArrayList<Quiz>res=new ArrayList<>();
-		  for(Object o:q) {
-				Quiz q1= modelMapper.map(o,Quiz.class);
-				res.add(q1);
-		  }
-	
-		   
-		 
-	   //return null;	
-	     return   quiz.addQuiz(res);	 
-	 }
-	 @PostMapping("/addQuizJson")
-	 public List<Quiz>addQuizJson(@RequestBody ArrayList<Quiz>q){
+@CrossOrigin({"http://localhost:3000","https://quizappsrpring.netlify.app/","https://quizappbackend-production-af37.up.railway.app/"})
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	UserService user;
+   @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User u) {
+	   
+	    
+	     User newuser= user.loginUser(u.email,u.password);
+	     System.out.println(newuser);	
+	      return ResponseEntity.ok(newuser);
+	   
+	   
+   }
+   @PostMapping("/register")
+   public ResponseEntity<?> register(@RequestBody User u) 
+   {
+	   
+	   
+	     User newuser= user.createUser(u);
+	     System.out.println(newuser);
+	     if(newuser==null)
+	     {
+	    	return ResponseEntity.ok("User Already exist !!Please change Username or Email");
+	     }
 	      
-		 return quiz.addQuiz(q);
-		// return quiz.addQuiz(al);
-	 }
-	 @GetMapping("/allCategories")
-	 public List<Quiz> getCategories(){
-		 
-		 
-		 List<Quiz>r= quiz.getCat();
-		   System.out.print(r);
-		   return r;	
-	 }
-	 @PostMapping("/submitQuiz")
-	  public String submitQuiz(@RequestBody Map<String,Object> req) {
-		 
-		 System.out.println(req	);
-		  int userId=Integer.parseInt(req.get("userId").toString());
-		  int  score=Integer.parseInt(req.get("score").toString());
-		  String quizCat=req.get("category").toString();
-		  	
-		  
-		   
-		 return quiz.submitQuiz(userId,score,quizCat);
-		 
-		 
-	 }
-	 @GetMapping("/allQuestions")
-	 public List<Quiz> getAllQuestions(){
-		    return quiz.getAllQuiz();
-	 }
-	 
-	 
-
+	     return ResponseEntity.ok(newuser);
+   }
+   @GetMapping("/results/{id}")
+   public ResponseEntity<?> quizResults(@PathVariable("id")int id){
+	        try 
+	        {
+	        	 System.out.println("userId:"+id);
+	        	 List<UserQuiz> u= user.getResults(id);
+                 return ResponseEntity.ok(u);
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Errror"+e);
+				return null;
+			}
+	      }
+   @GetMapping("/allUsers")
+    public ResponseEntity<?> allUsers(){
+	  List<User>li=user.allUsers();
+	   return ResponseEntity.ok(li); 
+   }
+   @PostMapping("/admin")
+    public ResponseEntity<?> addAdmin(@RequestBody User u) {
+	   
+	    User us=user.insertAdmin(u);	
+	    return ResponseEntity.ok(us);
+   }
 }
